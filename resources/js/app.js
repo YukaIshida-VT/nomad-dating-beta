c5870e8b27c57297c557c715670d3ada67a73d77
Yuka Ishida <ishida.yuka@valuence.inc>
2023-10-22 00:15:34 +0900

    viteとvueセットアップ

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import{ createVuetify } from 'vuetify'
+import App from "../views/App.vue"
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
+import '../css/app.css';
+
+const app = createApp(App);
+const vuetify = createVuetify();
+
+app.use(vuetify) 
+app.mount('#app');
