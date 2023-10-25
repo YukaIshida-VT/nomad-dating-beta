687dc584f58e80c1d21ceb48d907616d53c2342c
Yuka Ishida <ishida.yuka@valuence.inc>
2023-10-25 22:52:25 +0900

    UsersTableSeeder準備

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,14 +17,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $email = fake()->unique()->safeEmail();
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $email,
             'twitter_id' => Str::random(10),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'avator' => 'http://pbs.twimg.com/profile_images/1713579089368600576/JVU-JX-1_normal.jpg',
+            'avator' => Gravatar::get($email),
         ];
     }
 
