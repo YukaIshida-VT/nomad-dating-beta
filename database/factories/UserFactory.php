9a402cee0d9a74a07564e41fbbcfcddb93296401
Yuka Ishida <ishida.yuka@valuence.inc>
2023-10-25 23:03:39 +0900

    UserFactory修正

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Creativeorange\Gravatar\Facades\Gravatar;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -20,12 +21,13 @@ class UserFactory extends Factory
         $email = fake()->unique()->safeEmail();
         return [
             'name' => fake()->name(),
+            'nickname' => fake()->name(),
             'email' => $email,
             'twitter_id' => Str::random(10),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-            'avator' => Gravatar::get($email),
+            'api_token' => Str::random(32),
+            'avatar' => Gravatar::get($email),
         ];
     }
 
